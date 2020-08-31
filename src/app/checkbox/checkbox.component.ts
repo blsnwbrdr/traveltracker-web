1080f5fced54f28a344dea0823c348138bf6bdb0
Brandon Lew <brandon.t.lew@gmail.com>
2020-08-31 15:31:30 -0400

    created list of checkboxes and output selected to an array

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckboxComponent implements OnInit {
 
-  constructor() { }
+  countries = [
+    {
+      "name": "Aruba",
+      "continent": "North America",
+      "latlng": [
+        "12.5",
+        "-69.96666666"
+      ]
+    },
+    {
+      "name": "Afghanistan",
+      "continent": "Asia",
+      "latlng": [
+        "33",
+        "65"
+      ]
+    }
+  ];
+
+  selectedCountries: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.selectedCountries = this.fb.group({
+      userSelectedCountries: this.fb.array([])
+    });
   }
 
+  onChange(name: string, isChecked: boolean){
+
+    const selectedCountriesArray = <FormArray>this.selectedCountries.controls.userSelectedCountries;
+
+    if(isChecked){
+      selectedCountriesArray.push(new FormControl(name));
+    }else{
+      let index = selectedCountriesArray.controls.findIndex(x => x.value == name);
+      selectedCountriesArray.removeAt(index);
+    }
+
+    console.log(selectedCountriesArray.value);
+
+
+  }
 }
