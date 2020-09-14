b49bf19c083d7d6a2fa4c3435a33303ea71fe8ff
Brandon Lew <brandon.t.lew@gmail.com>
2020-09-14 14:23:17 -0400

    gather, list, and style user selections from local data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  selectedCountries = [];
 
-  constructor() { }
+  constructor(
+    private localStorageService: LocalStorageService
+  ) { }
 
   ngOnInit(): void {
+    // if there is local storage data, update selectedCountries array
+    if(this.localStorageService.get('data') !== null){
+      let localStorage = this.localStorageService.get('data');
+      // sort selected countries alphabetically
+      localStorage.sort((a,b) => {
+        if (a < b)
+          return -1;
+        if (a > b)
+          return 1;
+        return 0;
+      });
+      this.selectedCountries = localStorage;
+    }
   }
 
 }
