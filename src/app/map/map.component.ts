4a34de7c2c8f4c65ce016b51ff9493c96cd09379
Brandon Lew <brandon.t.lew@gmail.com>
2020-09-15 14:49:36 -0400

    fit map to browser dimensions and center on current location

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  height = '100%';
+  width = '100%';
+  zoom = 3;
+  center: google.maps.LatLngLiteral
+  options: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    zoomControl: true,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 10,
+    minZoom: 2,
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    })
   }
 
 }
