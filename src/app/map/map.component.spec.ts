784e38a435b7740e02a3a765c180a0993097c3f2
Brandon Lew <brandon.t.lew@gmail.com>
2023-02-03 11:57:04 -0500

    update map test

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+// COMPONENTS
 import { MapComponent } from './map.component';
+
+// SERVICES
+import { CountriesService } from '../services/countries.service';
+import { LocalStorageService } from '../services/local-storage.service';
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -8,7 +15,10 @@ describe('MapComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      declarations: [ MapComponent ],
+      imports: [HttpClientTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [CountriesService, LocalStorageService],
     })
     .compileComponents();
   }));
