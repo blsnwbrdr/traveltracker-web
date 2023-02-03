1b7ba07af7414f123164b6e007bb41c94d4a700f
Brandon Lew <brandon.t.lew@gmail.com>
2023-02-03 14:13:43 -0500

    format and fix linting problems

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MapMarker } from '@angular/google-maps'
 
 // INTERFACES AND SERVICES
 import { Countries } from '../interfaces/countries';
@@ -9,14 +8,14 @@ import { LocalStorageService } from '../services/local-storage.service';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
   // GOOGLE MAP VARIABLES
   height = '100%';
   width = '100%';
   zoom = 3;
-  center: google.maps.LatLngLiteral
+  center: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
     mapTypeId: 'roadmap',
     zoomControl: true,
@@ -24,7 +23,7 @@ export class MapComponent implements OnInit {
     disableDoubleClickZoom: true,
     maxZoom: 10,
     minZoom: 2,
-  }
+  };
   markers = [];
   // DATA VARIABLES
   errorMessage = '';
@@ -34,7 +33,7 @@ export class MapComponent implements OnInit {
   constructor(
     private countriesService: CountriesService,
     private localStorageService: LocalStorageService
-  ){}
+  ) {}
 
   ngOnInit(): void {
     // get current position for map centering
@@ -42,31 +41,31 @@ export class MapComponent implements OnInit {
       this.center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
-      }
-    })
+      };
+    });
     // if there is local storage data, update selectedCountries array with data
-    if(this.localStorageService.get('Visited') !== null){
+    if (this.localStorageService.get('Visited') !== null) {
       this.selectedCountries = this.localStorageService.get('Visited');
     }
     // import countries json data
     this.countriesService.getCountries().subscribe({
-      next: countries => {
+      next: (countries) => {
         // push lat/lng positions to markers array
-        for(const countriesKey of countries){
-          for(const selectedCountriesKey of this.selectedCountries){
-            if(countriesKey.name === selectedCountriesKey){
+        for (const countriesKey of countries) {
+          for (const selectedCountriesKey of this.selectedCountries) {
+            if (countriesKey.name === selectedCountriesKey) {
               this.markers.push({
                 position: {
                   lat: Number(countriesKey.latlng[0]),
-                  lng: Number(countriesKey.latlng[1])
-                }
-              })
+                  lng: Number(countriesKey.latlng[1]),
+                },
+              });
             }
           }
         }
         this.countries = countries;
       },
-      error: err => this.errorMessage = err
+      error: (err) => (this.errorMessage = err),
     });
   }
 }
--- src/app/sharing/sharing.component.ts
+++ src/app/sharing/sharing.component.ts
@@ -3,15 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sharing',
   templateUrl: './sharing.component.html',
-  styleUrls: ['./sharing.component.scss']
+  styleUrls: ['./sharing.component.scss'],
 })
 export class SharingComponent implements OnInit {
   usernameInputDisplay = true;
 
-  constructor() { }
-
   ngOnInit(): void {
-    // this.usernameInputDisplay = false;
+    console.log(this.usernameInputDisplay);
   }
-
 }
