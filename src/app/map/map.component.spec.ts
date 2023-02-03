83f36e6ec74f5df85a8148f6888154f60498277a
Brandon Lew <brandon.t.lew@gmail.com>
2023-02-03 13:18:49 -0500

    remove services from map

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -5,22 +5,16 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 // COMPONENTS
 import { MapComponent } from './map.component';
 
-// SERVICES
-import { CountriesService } from '../services/countries.service';
-import { LocalStorageService } from '../services/local-storage.service';
-
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ],
+      declarations: [MapComponent],
       imports: [HttpClientTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [CountriesService, LocalStorageService],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
