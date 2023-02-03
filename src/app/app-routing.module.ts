d4ad49e2b5e602d7399019bf18a9be8bc504c542
Brandon Lew <brandon.t.lew@gmail.com>
2023-02-02 20:45:02 -0500

    migrate to v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     GoogleMapsModule,
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, {})
   ],
   exports: [RouterModule]
 })
