c5ed4adc83f99123da248c01a55316ffdce43f22
Brandon Lew <brandon.t.lew@gmail.com>
2023-02-20 21:10:08 -0500

    create LocalStorageService method unit tests

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -4,14 +4,40 @@ import { TestBed } from '@angular/core/testing';
 import { LocalStorageService } from './local-storage.service';
 
 describe('LocalStorageService', () => {
-  let service: LocalStorageService;
+  let localStorageService: LocalStorageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LocalStorageService);
+    localStorageService = TestBed.inject(LocalStorageService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(localStorageService).toBeTruthy();
+  });
+
+  it('#get should be called and return value', () => {
+    spyOn(Object.getPrototypeOf(localStorage), 'getItem');
+    const returnValue = localStorageService.get('');
+    expect(Object.getPrototypeOf(localStorage).getItem).toHaveBeenCalled();
+    expect(returnValue).toBeTruthy();
+  });
+
+  it('#set should be called and return value', () => {
+    spyOn(Object.getPrototypeOf(localStorage), 'removeItem');
+    const returnValue = localStorageService.remove('');
+    expect(Object.getPrototypeOf(localStorage).removeItem).toHaveBeenCalled();
+    expect(returnValue).toBeTruthy();
+  });
+
+  it('#remove should be called and return value', () => {
+    spyOn(Object.getPrototypeOf(localStorage), 'setItem');
+    const returnValue = localStorageService.set('', []);
+    expect(Object.getPrototypeOf(localStorage).setItem).toHaveBeenCalled();
+    expect(returnValue).toBeTruthy();
+  });
+
+  it('#isLocalStorageSupported should return value', () => {
+    const returnValue = localStorageService.isLocalStorageSupported;
+    expect(returnValue).toBeTruthy();
   });
 });
