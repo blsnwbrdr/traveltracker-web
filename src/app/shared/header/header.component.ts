bdf440b04fc79cefa70cc47b309ae16577c740b6
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2024-08-30 21:43:19 -0700

    fixed header logic and css

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isScrolling = false;
+
+  @HostListener('window:scroll', ['event'])
+  onScroll() {
+    if (window.scrollY === 0) {
+      this.isScrolling = false;
+    } else if (window.scrollY > 0) {
+      this.isScrolling = true;
+    }
+  }
+}
